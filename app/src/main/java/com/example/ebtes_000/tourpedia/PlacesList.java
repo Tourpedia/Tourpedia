17989c8e367581ad2503336f03205055956ecff0
shaimaalzahrani <shimaalzhrani@gmail.com>
2016-03-25 15:06:18 +0300

    integrating guide me function version1

---
+++ app/src/main/java/com/example/ebtes_000/tourpedia/PlacesList.java
@@ -0,0 +1,17 @@
+package com.example.ebtes_000.tourpedia;
+
+import java.io.Serializable;
+import java.util.List;
+
+/** Implement this class from "Serializable"
+* So that you can pass this class Object to another using Intents
+* Otherwise you can't pass to another actitivy
+* */
+public class PlacesList implements Serializable {
+
+	public String status;
+
+
+	public List<Place> results;
+
+}
