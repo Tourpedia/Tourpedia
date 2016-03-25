7d869e643623470d6102289d402e16afb38d8390
shaimaalzahrani <shimaalzhrani@gmail.com>
2016-03-25 11:52:14 +0300

    plan connections and (home,settings,filters) for all *.*

--- app/src/main/java/com/example/ebtes_000/tourpedia/addPlan.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/addPlan.java
@@ -1,7 +1,10 @@
 package com.example.ebtes_000.tourpedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class addPlan extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class addPlan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();   // to hide the actionBar
         setContentView(R.layout.activity_add_plan);
+
+        ImageButton home = (ImageButton) findViewById(R.id.homeBtn);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(addPlan.this, home.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton setting = (ImageButton) findViewById(R.id.settingsBtn);
+        ImageButton filters = (ImageButton) findViewById(R.id.filterBtn);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(addPlan.this, settings.class);
+                startActivity(intent);
+
+
+            }
+        });
+        filters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(addPlan.this, filter.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ebtes_000/tourpedia/plans.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/plans.java
@@ -1,7 +1,10 @@
 package com.example.ebtes_000.tourpedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class plans extends AppCompatActivity {
 
@@ -10,5 +13,111 @@ public class plans extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_plans);
+
+        ImageButton add = (ImageButton) findViewById(R.id.addPlanBtn);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, addPlan.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton suggest = (ImageButton) findViewById(R.id.suggestPlanBtn);
+        suggest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, addPlan.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton edit = (ImageButton) findViewById(R.id.editPlanBtn);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, addPlan.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton delete = (ImageButton) findViewById(R.id.deletePlanBtn);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, addPlan.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton home = (ImageButton) findViewById(R.id.homeBtn);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, home.class);
+                startActivity(intent);
+
+
+            }
+        });
+        ImageButton setting = (ImageButton) findViewById(R.id.settingsBtn);
+        ImageButton filters = (ImageButton) findViewById(R.id.filterBtn);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, settings.class);
+                startActivity(intent);
+
+
+            }
+        });
+        filters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(plans.this, filter.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
