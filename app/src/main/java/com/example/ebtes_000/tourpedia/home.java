be539abb0a24ec2b826e0c8f43604c4eb91bdd6f
shaimaalzahrani <shimaalzhrani@gmail.com>
2016-03-25 11:23:38 +0300

    home connections are done *.*

--- app/src/main/java/com/example/ebtes_000/tourpedia/home.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/home.java
@@ -19,6 +19,8 @@ public class home extends AppCompatActivity {
         getSupportActionBar().hide();   // to hide the actionBar
         setContentView(R.layout.activity_home);
         ImageButton guideMe = (ImageButton) findViewById(R.id.guideBtn);
+        ImageButton Plan = (ImageButton) findViewById(R.id.planBtn);
+        ImageButton idntify = (ImageButton) findViewById(R.id.identifyBtn);
         guideMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +29,35 @@ public class home extends AppCompatActivity {
                 intent.setClass(home.this, guideMe.class);
                 intent.putExtra("Bitmap", bitmap);
                 startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(home.this, guideMe.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(home.this, plans.class);
+                startActivity(intent);
+
+
+            }
+        });
+        idntify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
+                Intent intent = new Intent();
+                intent.setClass(home.this, guideMe.class);
+                intent.putExtra("Bitmap", bitmap);
+                startActivity(intent);*/ // causes faild binder
+                Intent intent = new Intent(home.this, camera.class);
                 startActivity(intent);
 
 
