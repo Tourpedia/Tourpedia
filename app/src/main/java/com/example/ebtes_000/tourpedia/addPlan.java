a53b97c96dd341dc99a6fc8e30276542299b7131
ebtisamSH <ebtesam.sh@hotmail.com>
2016-04-14 08:46:34 +0300

    Plan function

--- app/src/main/java/com/example/ebtes_000/tourpedia/addPlan.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/addPlan.java
@@ -1,63 +1,226 @@
 package com.example.ebtes_000.tourpedia;
 
+import android.app.DialogFragment;
+
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 
 public class addPlan extends AppCompatActivity {
-
+    String planName = "p";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();   // to hide the actionBar
         setContentView(R.layout.activity_add_plan);
 
+        // to home
         ImageButton home = (ImageButton) findViewById(R.id.homeBtn);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(addPlan.this, home.class);
                 startActivity(intent);
-
-
             }
         });
+
         ImageButton setting = (ImageButton) findViewById(R.id.settingsBtn);
         ImageButton filters = (ImageButton) findViewById(R.id.filterBtn);
+
+        //to settings
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(addPlan.this, settings.class);
                 startActivity(intent);
-
-
             }
         });
+
+        //to filters
         filters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(addPlan.this, filter.class);
                 startActivity(intent);
-
-
             }
         });
+
+
+    }
+    // to show date picker from the edit text
+   public void showDatePickerDialog(View v) { // some examples use (onStart then OnfoucseChange)
+       EditText planDateTxt = (EditText) findViewById(R.id.planDate);
+       DatePickerFragment datePickerFragment = new DatePickerFragment(v);
+       FragmentTransaction ft = getFragmentManager().beginTransaction();
+       datePickerFragment.show(ft, "DatePicker");
+       // DialogFragment newFragment = new DatePickerFragment();
+        //newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) { // some examples use (onStart then OnfoucseChange)
+        EditText planDateTxt = (EditText) findViewById(R.id.timeFrom);
+        TimePickerFragment timePickerFragment = new TimePickerFragment(v);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        timePickerFragment.show(ft, "DatePicker");
+        // DialogFragment newFragment = new DatePickerFragment();
+        //newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void addEvent (View v) {
+
+        // stupid way to avoid redundancy when adding multiple events
+        if (planName.equals("p")) {
+            // plan information
+            EditText pName = (EditText) findViewById(R.id.planName);
+            EditText planDate = (EditText) findViewById(R.id.planDate);
+            planName = pName.getText().toString();
+            String date = planDate.getText().toString();
+            // event information
+            EditText placeName = (EditText) findViewById(R.id.placeTxt);
+            EditText from = (EditText) findViewById(R.id.timeFrom);
+            EditText to = (EditText) findViewById(R.id.timeTo);
+            String place = placeName.getText().toString();
+            String timeFrom = from.getText().toString();
+            String timeTo = to.getText().toString();
+
+
+
+            try {
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(planName, MODE_PRIVATE));
+                //   FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+                // writing name
+                outputStreamWriter.write(planName + "\n");
+                // writing date
+                outputStreamWriter.write(date + "\n");
+
+                outputStreamWriter.write(place + "\n");
+                outputStreamWriter.write("From: " + timeFrom);
+                outputStreamWriter.write(" - To: " + timeTo + "\n");
+
+                outputStreamWriter.close();
+                Toast.makeText(getApplicationContext(), "Event Added", Toast.LENGTH_LONG).show();
+                // clearing information
+                placeName.getText().clear();
+                placeName.setHint("Place to go");
+                from.getText().clear();
+                from.setHint("From");
+                to.getText().clear();
+                to.setHint("To");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            EditText placeName = (EditText) findViewById(R.id.placeTxt);
+            EditText from = (EditText) findViewById(R.id.timeFrom);
+            EditText to = (EditText) findViewById(R.id.timeTo);
+            String place = placeName.getText().toString();
+            String timeFrom = from.getText().toString();
+            String timeTo = to.getText().toString();
+            try {
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(planName, MODE_APPEND));
+                //   FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+
+                outputStreamWriter.write(place + "\n");
+                outputStreamWriter.write("From: " + timeFrom);
+                outputStreamWriter.write(" - To: " + timeTo + "\n");
+
+                outputStreamWriter.close();
+                Toast.makeText(getApplicationContext(), "Event Added", Toast.LENGTH_LONG).show();
+                // clearing information
+                placeName.getText().clear();
+                placeName.setHint("Place to go");
+                from.getText().clear();
+                from.setHint("From");
+                to.getText().clear();
+                to.setHint("To");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }// end else
+    }
+
+    public void savePlan(View view){
+        // plan information
+        EditText pName = (EditText) findViewById(R.id.planName);
+        EditText planDate = (EditText) findViewById(R.id.planDate);
+        planName = pName.getText().toString();
+        String date = planDate.getText().toString();
+        // event information
+        EditText placeName = (EditText) findViewById(R.id.placeTxt);
+        EditText from = (EditText) findViewById(R.id.timeFrom);
+        EditText to = (EditText) findViewById(R.id.timeTo);
+        String place = placeName.getText().toString();
+        String timeFrom = from.getText().toString();
+        String timeTo = to.getText().toString();
+
+        // specifying file name
+        //String fileName = name;
+
+        try {
+           OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(planName, MODE_PRIVATE));
+         //   FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            // writing name
+            outputStreamWriter.write(planName+"\n");
+            // writing date
+            outputStreamWriter.write(date+"\n");
+
+            outputStreamWriter.write(place+"\n");
+            outputStreamWriter.write("From: "+timeFrom);
+            outputStreamWriter.write(" - To: "+timeTo+"\n");
+
+            outputStreamWriter.close();
+            Toast.makeText(getApplicationContext(), "Plan saved", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+///////////////////////////////////
+        try {
+            String Readdate;
+          //  getFilesDir();
+            FileInputStream fileInputStream = openFileInput(planName);
+            InputStreamReader inputSreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputSreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((Readdate=bufferedReader.readLine()) != null)
+            {
+                stringBuffer.append(Readdate + "\n");
+            }
+
+            TextView textView = (TextView) findViewById(R.id.date);
+            textView.setText(stringBuffer.toString()+getFilesDir());
+            textView.setVisibility(View.VISIBLE);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- app/src/main/java/com/example/ebtes_000/tourpedia/plans.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/plans.java
@@ -4,9 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class plans extends AppCompatActivity {
+    ListView listSavedPlans;
+    String[] SavedPlans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,110 +26,123 @@ public class plans extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_plans);
 
+        // declare add plan img button
         ImageButton add = (ImageButton) findViewById(R.id.addPlanBtn);
+        // to add plan
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, addPlan.class);
                 startActivity(intent);
-
-
             }
         });
+        // declare suggest plan img button
         ImageButton suggest = (ImageButton) findViewById(R.id.suggestPlanBtn);
+        // to suggest plan
         suggest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, addPlan.class);
                 startActivity(intent);
-
-
             }
         });
-        ImageButton edit = (ImageButton) findViewById(R.id.editPlanBtn);
+
+    /*    ImageButton edit = (ImageButton) findViewById(R.id.editPlanBtn);
+        // to edit plan
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, addPlan.class);
                 startActivity(intent);
-
-
             }
         });
+
         ImageButton delete = (ImageButton) findViewById(R.id.deletePlanBtn);
+        // to delete plan
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, addPlan.class);
                 startActivity(intent);
-
-
             }
         });
+*/
         ImageButton home = (ImageButton) findViewById(R.id.homeBtn);
+        // to home
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, home.class);
                 startActivity(intent);
-
-
             }
         });
+
+        // declaring settings and filters img buttons
         ImageButton setting = (ImageButton) findViewById(R.id.settingsBtn);
         ImageButton filters = (ImageButton) findViewById(R.id.filterBtn);
+
+        // to settings
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, settings.class);
                 startActivity(intent);
-
-
             }
         });
+
+        // to filters
         filters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(plans.this, filter.class);
                 startActivity(intent);
+            }
+        });
 
+        listSavedPlans = (ListView)findViewById(R.id.list);
+        ShowSavedFiles();
+
+        listSavedPlans.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // Intent intent = new Intent(plans.this, PlanDetails.class);
+                //startActivity(intent);
+                String planName = (String) listSavedPlans.getItemAtPosition(position);
+                try {
+                    String Readdate;
+                    //  getFilesDir();
+                    FileInputStream fileInputStream = openFileInput(planName);
+                    InputStreamReader inputSreamReader = new InputStreamReader(fileInputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputSreamReader);
+                    StringBuffer stringBuffer = new StringBuffer();
+                    while ((Readdate=bufferedReader.readLine()) != null)
+                    {
+                        stringBuffer.append(Readdate + "\n");
+                    }
+
+                    TextView textView = (TextView) findViewById(R.id.date);
+                    textView.setText(stringBuffer.toString());
+                    textView.setVisibility(View.VISIBLE);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
+
+
+    }
+    void ShowSavedFiles(){
+        SavedPlans = getApplicationContext().fileList();
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                SavedPlans);
+
+        listSavedPlans.setAdapter(adapter);
+
     }
 }
