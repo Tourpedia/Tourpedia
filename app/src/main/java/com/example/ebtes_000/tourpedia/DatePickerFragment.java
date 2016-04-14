7bd0e03a5247f39a35bd80e47ebe3bfdc7e1a680
ebtisamSH <ebtesam.sh@hotmail.com>
2016-04-14 11:59:36 +0300

    Plan function, with DatePicker and TimePicker

---
+++ app/src/main/java/com/example/ebtes_000/tourpedia/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.example.ebtes_000.tourpedia;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+/**
+ * Created by ebtes_000 on 09/04/16.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+    EditText dateTxt;
+    public DatePickerFragment(View view){ // there was a problem here
+       dateTxt = (EditText)view;
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+       dateTxt.setText(day + "/" + (month + 1) + "/" + year);
+    }
+}
