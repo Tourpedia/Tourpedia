2ed28c898790c378162f469a914580f0f5215d3c
bashayer <Bashayer.65@gmail.com>
2016-04-23 10:55:51 +0300

    Enclude Setting and Filter classes , along with intil blind class

---
+++ app/src/main/java/com/example/ebtes_000/tourpedia/blind.java
@@ -0,0 +1,113 @@
+package com.example.ebtes_000.tourpedia;
+
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.speech.RecognizerIntent;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Locale;
+import android.widget.ArrayAdapter;
+
+
+/**
+ * Created by bashayer on 22/04/2016.
+ */
+public class blind extends AppCompatActivity {
+
+    private ListView lv;
+    static final int check = 1111;
+    static String [] texts = {"Welcome to Tourpedia, What do you want to do?"
+             , "Identify landmarks" , "Guidance" ,"Make a plan" , "Setting", "Filter"};
+    String [] guide = {"Attraction","Restaurant","Restroom","Taxi-Stand"};
+    String [] plan = {"add plan","edit plan", "delete plan"};
+    TextToSpeech tts ;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();   // to hide the actionBar
+        setContentView(R.layout.blind);
+
+        lv = (ListView) findViewById(R.id.lvVoiceReturn);
+        ImageButton record = (ImageButton) findViewById(R.id.recordButton);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                i.putExtra(RecognizerIntent.EXTRA_PROMPT , "Speak !");
+                startActivityForResult(i, check);
+            }
+        });
+
+
+        Button listen = (Button) findViewById(R.id.bTextToVoice);
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < texts.length ; i++)
+                    ConvertToSpeech(texts[i]);
+            }
+        });
+
+        tts = new TextToSpeech(blind.this, new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.ENGLISH);}
+            }
+        });
+
+    }//onCreate end
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == check && resultCode == RESULT_OK) {
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , results));
+            String str = results.get(0);
+            switch (str) {
+                case "Identify landmarks":
+                    // call identify class
+                    break;
+                case "Guidance":
+                    tts.speak("which type of places you want?",TextToSpeech.QUEUE_FLUSH , null);
+                    // call guide arr
+
+                    break;
+                case "Make a plan":
+                    tts.speak("what do you want?",TextToSpeech.QUEUE_FLUSH , null);
+                    // call plan arr
+                    break;
+                case "Setting":
+
+                    break;
+                case "Filter":
+                    break;
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    }
+
+    void ConvertToSpeech(String str) {
+        tts.speak(str , TextToSpeech.QUEUE_FLUSH , null);
+    }
+
+
+}
--- app/src/main/java/com/example/ebtes_000/tourpedia/filter.java
+++ app/src/main/java/com/example/ebtes_000/tourpedia/filter.java
@@ -3,13 +3,25 @@ package com.example.ebtes_000.tourpedia;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class filter extends AppCompatActivity {
+    private int distanceAtt , ratingAtt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,54 +33,139 @@ public class filter extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(filter.this, home.class);
                 startActivity(intent);
-
-
             }
         });
+
         ImageButton setting = (ImageButton) findViewById(R.id.settingsBtn);
         ImageButton filters = (ImageButton) findViewById(R.id.filterBtn);
+
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(filter.this, settings.class);
                 startActivity(intent);
-
-
             }
         });
+
         filters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.guideme1 );
-                Intent intent = new Intent();
-                intent.setClass(home.this, guideMe.class);
-                intent.putExtra("Bitmap", bitmap);
-                startActivity(intent);*/ // causes faild binder
                 Intent intent = new Intent(filter.this, filter.class);
                 startActivity(intent);
+            }
+        });
 
+        final EditText distanceText = (EditText) findViewById(R.id.distance);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        Button submit = (Button) findViewById(R.id.submitButton);
+     //   double rate = ratingBar.getRating();
+
+
+        distanceText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                Log.d("res1", "rating:" + s.toString());
+                distanceAtt = Integer.parseInt(s.toString());
 
             }
         });
 
-        RatingBar rating = (RatingBar) findViewById(R.id.ratingBar);
-        EditText distance = (EditText) findViewById(R.id.distance);
-        double rate = rating.getRating();
-        //int distanceM = Integer.parseInt(String.valueOf(distance.getText()));
+        //if rating value is changed
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
 
-        Log.d("res1","rating:"+rate);
+                Log.d("res1", "rating:" + rating);
+                ratingAtt = (int) rating;
 
+            }
+        });
+
+
+        //if click on me, then display the current rating value.
+        submit.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          // save to file
+                                          saveFilterToFile();
+                                      }
+
+                                  }
+
+        );
+
+
+        }// onCreate end
+
+    void saveFilterToFile() {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("Filter.txt", MODE_PRIVATE));
+
+            outputStreamWriter.write(distanceAtt+"\n");
+            outputStreamWriter.write(ratingAtt + "\n");
+
+            outputStreamWriter.close();
+        }  // try
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }// saveFile end
+
+
+    public filter() {
+        retriveFilterFromFile();
+    }
+    
+      void retriveFilterFromFile() {
+        try {
+            String settingName;
+            int lineNum=1;
+
+            FileInputStream fileInputStream = openFileInput("Filter.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            Log.d("trace1", "after stream");
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while ((settingName = bufferedReader.readLine()) != null) {
+                Log.d("trace1",lineNum+"");
+                if (lineNum == 1){
+                    Log.d("trace1", "inside if 1");
+                    distanceAtt = Integer.parseInt(settingName);
+                }
+                else if (lineNum == 2) {
+                    Log.d("trace1","inside if 2");
+                    ratingAtt = Integer.parseInt(settingName);
+                }
+
+                lineNum++;
+            }//while
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//retriveFrom end
+
+    public int getDistance() {
+        return distanceAtt;
+    }
+
+    public int getRating() {
+        return ratingAtt;
     }
 }
